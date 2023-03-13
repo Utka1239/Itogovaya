ee3dcb6bb334289872a46f99e15a190f16293e8c
Utka1239 <samsitov@list.ru>
2023-03-13 21:26:40 +0500

    second commit

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 string[] array = Array();
 string[] result = Find(array, 3);
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] Find(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
